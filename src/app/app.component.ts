0a354216484143daf3e9ef75dfad050dea1d37b4
kxrthiknair <93530074+kxrthiknair@users.noreply.github.com>
2023-08-31 16:25:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'procademyForms';
+  defaultCountry = 'india';
+  firstname:string ='';
+  lastname:string ='';
+  defaultGen = 'Male';
+  email:string ='';
+  gen:string ='';
+  country:string='';
+
+
+  gender = [
+    {id:'1', value: 'Male'},
+    {id:'2' , value:'Female'},
+    {id:'3' , value:'Other'}
+  ];
+
+  @ViewChild('myForm') form?: NgForm;
+
+
+
+  onSubmit(){
+    console.log(this.form);
+
+    this.firstname = this.form?.value.personalDetails.firstname;
+    this.lastname = this.form?.value.personalDetails.lastname;
+    this.email = this.form?.value.personalDetails.email;
+    this.gen = this.form?.value.gender;
+    this.country = this.form?.value.country;
+
+
+
+    this.form?.reset();
+  }
+
+  setDfaultValues(){
+    // this.form!.value.personalDetails.firstname = 'Karthik';
+    // this.form!.value.personalDetails.lastname = 'Nair';
+    // this.form!.value.personalDetails.email = 'herbolax77@gmail.com';
+
+    // this.form!.setValue({
+    //   country:'',
+    //   gender:'',
+    //   hobbies:'',
+    //   personalDetails:{
+    //     email:'herbolax77@gmail.com',
+    //     firstname:'Karthik',
+    //     lastname:'Nair'
+    //   }
+    // })
+
+    this.form?.form.patchValue({
+      personalDetails: {
+        email:'herbolax78@gmail.com',
+        firstname:'karthik',
+        lastname:'nair'
+      }
+
+    })
+  }
+
+
 }
+
